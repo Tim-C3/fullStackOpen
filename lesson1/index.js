b21990ebf3f1de2ca0fb12a818ef6b72b09cb8d2
Tim-C3 <87870513+Tim-C3@users.noreply.github.com>
2022-10-20 10:32:12 +0100

    done something

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,8 +1,9 @@
-const { request } = require('express')
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
 
+app.use(cors())
 
 let notes = [
     {
@@ -47,11 +48,9 @@ let notes = [
     }
   })
 
-
   app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
-  
     response.status(204).end()
   })
 
@@ -65,15 +64,11 @@ let notes = [
 
   app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    
-
     if (!body.content) {
         return response.status(400).json({
             error: "content missing"
         })
     }
-    
     const note = {
         content: body.content,
         important: body.important || false,
@@ -81,17 +76,13 @@ let notes = [
         id: generateId()
 
     }
-    
-   
-    
+
     console.log(note)
-
     notes = notes.concat(note)
-
     response.json(note)
   })
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
