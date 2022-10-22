9ba496d64af594a3b7a23d9c857c2b44bbf40b88
Tim-C3 <87870513+Tim-C3@users.noreply.github.com>
2022-10-22 19:12:28 +0100

    d

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ const checkDuplicate = name => {
     }
 }
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
